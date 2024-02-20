39ccc735b2ed56b9f884f14e15b74563c1316dd6
jkouil <jkouil87451949@163.com>
2024-02-20 13:05:27 -0500

    q4 generator and verificator

--- Q4 Dobble/dobble_generator.py
+++ Q4 Dobble/dobble_generator.py
@@ -1,33 +1,163 @@
-#Nom, Matricule
-#Nom, Matricule
+# Qiwu Wen, Matricule
+# Nom, Matricule
 
 # cette classe sert a créer les cartes du jeu dans le fichier cartes.txt
 # this class is used to create the game cards in the cartes.txt file
 
-import random # pour le melange des symboles sur chaque carte # for mixing symbols on each card
+import random  # pour le melange des symboles sur chaque carte # for mixing symbols on each card
+
+import dobble_verificator
+
 
 class Generator():
-    def __init__(self, order = 7):
+
+    def __init__(self, order=7):
         self.order = order
 
-    def generate(self, cards_file = "cartes.txt", verbose = False):
-        if verbose :
+    def generate(self, cards_file="cartes.txt", verbose=False):
+        if verbose:
             print("***Generation des cartes***")
 
         # TODO
         # a completer
+        def add_horizon(horizon, sym):
+            if len(horizon[n]) == n:  # dernier chiffre est rempli
+                for elem in horizon:
+                    elem.append(sym)  # ajoute symbol au n ieme elements
+            for elem in horizon:
+                if len(elem) < n:  # on peut encore ajouter symbol au carte
+                    elem.append(sym)
+
+                    return
+
+        def lists_verticals(n):
+            #retourner des listes des points(x,y) qui sont dans
+            #les memes lignes verticales
+            resultat = []
+            for x in range(n):
+                ligneX = []
+                for y in range(n):
+                    pos = (x,y)
+                    ligneX.append(pos)
+                resultat.append(ligneX)
+            return resultat
+
+        def lists_horizontals(n):
+            #retourner des listes des points(x,y) qui sont dans
+            #les memes lignes horizontales
+            resultat = []
+            for y in range(n):
+                ligneY = []
+                for x in range(n):
+                    pos = (x,y)
+                    ligneY.append(pos)
+                resultat.append(ligneY)
+            return resultat
+
+        def lists_directions(n, pente):
+            resultat = []
+            for degree in range(n):
+                dListe = []
+                for x in range(n):
+                    pos = (x, (pente * x + degree) % n)
+                    dListe.append(pos)
+
+                resultat.append(dListe)
+            return resultat
+
+        def mettre_syms(n,symbols,dictCartes, horizon):
+            listVer = lists_verticals(n)
+            listHor = lists_horizontals(n)
+
+            for ver in listVer:
+                sym = symbols.pop()
+                add_horizon(horizon,sym)
+                for position in ver:
+                    if position not in dictCartes.keys():
+                        dictCartes[position] = []
+                    dictCartes[position].append(sym)
+
+            for pente in range(1,n):
+                listPenteN = lists_directions(n,pente)
+                for list in listPenteN:
+                    sym = symbols.pop()
+                    add_horizon(horizon, sym)
+
+                    for position in list:
+                        if position not in dictCartes.keys():
+                            dictCartes[position] = []
+                        dictCartes[position].append(sym)
+
+            for hor in listHor:
+                sym = symbols.pop()
+                add_horizon(horizon, sym)
+                for position in hor:
+                    if position not in dictCartes.keys():
+                        dictCartes[position] = []
+                    dictCartes[position].append(sym)
+
+            add_horizon(horizon,symbols.pop())
+            return dictCartes
 
         # melange aleatoire des symboles sur les cartes,
         # pour ne pas avoir des répétitions de symboles sur les mêmes endroits des cartes
         # random mixing of symbols on the cards,
         # so as not to have repetitions of symbols on the same places on the cards
-            
-        # TODO
-        # a completer
 
+        def melanger(symbols):
+            random.shuffle(symbols)
+            return
 
         # ecriture des cartes dans le fichier cards_file
         # writing cards in the cards_file file
-            
+        def write(fileName, dictCartes,horizon):
+            file = open(fileName, "w")
+
+            for position in dictCartes: #position = carte
+                symbs = dictCartes[position] # liste des symbols par chaque carte
+                melanger(symbs)
+                for symb in symbs:
+                    file.write(str(symb) + " ")
+                file.write("\n")
+            for card in horizon:
+                melanger(card)
+                for sym in card:
+                    file.write(str(sym) + " ")
+                file.write("\n")
+            file.close()
+
+        n = self.order
+
+        nombreTotal = n ** 2 + n + 1
+
+        dictCartes = dict() # dict (i,j): carte[symbols]
+        horizon = [[] for _ in range(n+1)]  # tableau horizon [n+1]
+        symbols = [i+1 for i in range(nombreTotal)]  # tableau des symboles de longeur n**2 + n + 1
+        melanger(symbols)
+        # random.shuffle(symbols)
+        mettre_syms(n,symbols,dictCartes,horizon)
+
+        write(fileName="cartes.txt", dictCartes = dictCartes,horizon=horizon)
+
+
+
+
+      #  add_horizon(horizon, symbols)  # ajoute dernier elems du symbols au horizon
+
+       # print(horizon)
+
+
         # TODO
         # a completer
+
+
+
+        # TODO
+        # a completer
+
+
+#if __name__ == '__main__':
+ #   g = Generator()
+ #   v = dobble_verificator.Verificator()
+   # print(v.verify("cartes.txt",False))
+    #g.generate(g, False)
--- Q4 Dobble/dobble_verificator.py
+++ Q4 Dobble/dobble_verificator.py
@@ -1,5 +1,5 @@
-#Nom, Matricule
-#Nom, Matricule
+# Nom, Matricule
+# Nom, Matricule
 
 # cette classe sert a verifier la validite de l'ensemble des cartes du jeu dans le fichier cartes.txt
 # this class is used to check the validity  of the game cards set in the cartes.txt file
@@ -7,37 +7,85 @@
 # doit retourner 0 si tout est correct, 1 si le jeu n'est pas optimal selon l'ordre et 2 si le jeu n'est pas valide
 # should return 0 if everything is correct, 1 if the game set is not optimal according to the order and 2 if the game set is invalid
 
-#import os.path
+# import os.path
 
 class Verificator():
     def __init__(self):
         pass
 
-    def verify(self, cards_file = "cartes.txt", verbose = False):
-        if verbose :
+    def verify(self, cards_file="cartes.txt", verbose=False):
+        if verbose:
             print("***Verification des cartes***")
+
         # TODO
         # a completer
+        def check2cards(paires):
+            it = iter(paires)
+            for paireCard in it:
+
+                card1 = paireCard[0]
+                card2 = paireCard[1]
+                same_sym = 0  # nb de symbols identiques
+                for sym in card1:
+                    if sym in card2:
+                        same_sym += 1
+                if same_sym != 1:
+                    # print(card1,card2,same_sym)
+                    return False
+
+            return True
+
+        all_sym = set()
+        with open(cards_file) as file:
+            game = []
+            for line in file:
+                symbol_list = list(map(float, line.strip().split(" ")))
+                game.append(symbol_list)
+                all_sym.update(symbol_list)
+            file.close()
+        n = len(game[0]) - 1  # l'ordre du jeu
 
         # test : le nombre de carte devrait être optimal
+        test1 = False
+        nb_carte = len(game)
+        nb_expected = n ** 2 + n + 1
+        if nb_carte == nb_expected:
+            test1 = True
+
         # test : le nombre de symboles par carte est le même pour chaque carte
+        test2 = True
+        nombreSymb = len(game[0])
+        for card in game:
+            if nombreSymb != len(card):
+                test2 = False
+
         # test : chaque paire de cartes partagent toujours un et un seul symbole en commun
+        test3 = False
+        pairesCards = [(card1, card2) for card1 in game for card2 in game if card1 != card2]
+        if check2cards(pairesCards):
+            test3 = True
+
         # test : le nombre de symbole total devrait être optimal
-            
+        nb_total = len(all_sym)
+        test4 = False
+        if (nb_total == nb_expected):
+            test4 = True
+
         # test: the number of cards should be optimal
         # test: the number of symbols per card is the same for each card
         # test: each pair of cards always shares one and only one symbol in common
         # test: the total number of symbols should be optimal
 
-
+        if not test2 or not test3: return 2
+        if not test1 or not test4: return 1
 
         # succes (0) si le jeu est valide et optimal
         # avertissement (1) si le jeu de carte n'est pas optimal
         # erreur (2) si le jeu de carte n'est pas valide
-            
+
         # success (0) if the game is valid and optimal
         # warning (1) if the card game is not optimal
         # error (2) if the card set is invalid
         return 0
 
-        
+
